6b99a944c8f0fc9e71c7f76bf2633667f0f1a8a7
Cuzio <itomax19@gmail.com>
2023-06-28 15:47:59 +0100

    README file

--- public/app.js
+++ public/app.js
@@ -1,20 +1,27 @@
 const input = document.querySelector("#input");
 const btn = document.querySelector("#btn");
+const shortenUrl = document.querySelector("#shortenUrl");
 
 btn.addEventListener("click", async () => {
     console.log("clicked");
     const url = input.value;
     await getUrl(url);
+
 });
 
 
 const getUrl = async (site) => {
     try {
-        const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${site}/very/long/link.html`)
+        const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${site}`)
 
-        const data = response.json();
+        const data = await response.json();
+
         console.log(data.result.full_short_link);
+        
+        shortenUrl.innerHTML = data.result.full_short_link;
+
     } catch (error) {
         console.log(error);   
+        alert(error.message);
     }
 }
